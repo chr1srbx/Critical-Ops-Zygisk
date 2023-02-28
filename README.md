93e4744c0650b4b379035b1c759a6eaa5dc2fd1c
chr1srbx <accsbenny@gmail.com>
2023-02-28 17:06:43 +0200

    heehee

---
+++ README.md
@@ -0,0 +1,16 @@
+# ZygiskPG
+Mod Menu for Pixel Gun 3D 23.0.1
+
+This is NOT a good project 
+You WILL encounter many problems
+
+
+# Build and Installation
+Android Studio is required: https://developer.android.com/studio  <br />
+Use Android Studio to run the gradle task :module:assembleRelease to compile, the zip package will be generated in the out folder
+
+# Credits
+Perfare - https://github.com/Perfare/Zygisk-Il2CppDumper <br />
+springmusk026 - https://github.com/springmusk026/ImGui-Unity-With-Layout <br />
+jmpews - https://github.com/jmpews/Dobby <br />
+Polarmods - https://github.com/Polarmods/PolarImGui <br />
---
+++ module/src/main/cpp/hook.cpp
@@ -0,0 +1,204 @@
+#include <cstring>
+#include <cstdio>
+#include <unistd.h>
+#include <sys/system_properties.h>
+#include <dlfcn.h>
+#include <dlfcn.h>
+#include <cstdlib>
+#include <cinttypes>
+#include <string>
+#include <vector>
+#include <sstream>
+#include <fstream>
+#include <EGL/egl.h>
+#include <GLES2/gl2.h>
+#include "imgui.h"
+#include "imgui_internal.h"
+#include "backends/imgui_impl_opengl3.h"
+#include "backends/imgui_impl_android.h"
+#include "KittyMemory/KittyMemory.h"
+#include "KittyMemory/MemoryPatch.h"
+#include "KittyMemory/KittyScanner.h"
+#include "KittyMemory/KittyUtils.h"
+#include"Includes/Dobby/dobbyForHooks.h"
+#include "Include/Unity.h"
+#include "Misc.h"
+#include "hook.h"
+#include "Include/Roboto-Regular.h"
+
+#define GamePackageName "com.criticalforceentertainment.criticalops"
+
+/*
+ *
+ * LIST OF NOTES:
+ * GAMEMODES LIST: 丂丛万世丝七丒与丈, GameMode.cs << DONT TRUST THIS, ITS 丈下东与三丆下丛丄
+ * 丒丘丁丄丂丑丛丄东, ItemReward.cs (Scripts\\Store\\MainStore\\ItemReward.cs)
+ * 丒下丗丈丕三丌丏不, ProgressUpdater.Currency.cs (Scripts\\ServerProgressSync\\ProgressUpdater.Currency.cs)
+ * 丐三丐丄丝丅丈丄丞, OfferData.cs (Scripts/OfferPacks/OfferData.cs)
+ * SCENEINFO IS SceneInfo, FULLY OBFUSCATED
+ * SCENEINFO 且与丒与丈下丈丁一 = AddMode(GameMode)
+ *
+ */
+
+monoString* CreateIl2cppString(const char* str)
+{
+    static monoString* (*CreateIl2cppString)(const char* str, int *startIndex, int *length) =
+    (monoString* (*)(const char* str, int *startIndex, int *length))(g_il2cppBaseMap.startAddress + string2Offset(OBFUSCATE("0x44865B4")));
+    int* startIndex = 0;
+    int* length = (int *)strlen(str);
+    return CreateIl2cppString(str, startIndex, length);
+}
+
+static int selectedScene = 0;
+const char* sceneList[] = { "Fort", "Farm", "Hill", "Dust", "Mine", "Jail", "rust", "Gluk", "Cube", "City", "Pool", "Ants", "Maze", "Arena", "Train", "Day_D", "Space", "Pizza", "Barge", "Pool2", "Winter", "Area52", "Castle", "Arena2", "Sniper", "Day_D2", "Matrix", "Heaven", "office", "Portal", "Hungry", "Bridge", "Gluk_2", "knife2", "Estate", "Glider", "Utopia", "School", "Gluk_3", "spleef1", "Slender", "Loading", "temple4", "sawmill", "Parkour", "pg_gold", "olympus", "Stadium", "ClanWar", "shipped", "Coliseum", "GGDScene", "Paradise", "valhalla", "Assault2", "Training", "Speedrun", "Hospital", "Hungry_2", "mine_new", "LevelArt", "facility", "office_z", "Pumpkins2", "red_light", "BioHazard", "ChatScene", "impositor", "PromScene", "New_tutor", "Cementery", "AppCenter", "aqua_park", "Aztec_old", "ClanWarV2", "toy_story", "checkmate", "CustomInfo", "tokyo_3019", "new_hangar", "Pool_night", "china_town", "FortAttack", "Ghost_town", "Area52Labs", "Ice_Palace", "Arena_Mine", "SkinEditor", "North_Pole", "Ghost_town2", "Arena_Swamp", "ToyFactory3", "NuclearCity", "space_ships", "FortDefence", "Two_Castles", "Ships_Night", "RacingTrack", "Coliseum_MP", "Underwater2", "ChooseLevel", "Sky_islands", "Menu_Custom", "Secret_Base", "white_house", "ProfileShop", "Arena_Space", "Cube_portals", "ClosingScene", "Mars_Station", "Arena_Castle", "checkmate_22", "Hungry_Night", "Sky_islands2", "Death_Escape", "Arena_Hockey", "WinterIsland", "Dust_entering", "pizza_sandbox", "alien_planet2", "LevelComplete", "COLAPSED_CITY", "ClanTankBuild", "train_robbery", "space_updated", "AfterBanScene", "corporate_war", "ships_updated", "templ4_winter", "Pool_entering", "supermarket_2", "DuelArenaSpace", "LoadAnotherApp", "checkmate_22.0", "Paradise_Night", "Slender_Multy2", "Code_campaign3", "Spleef_Arena_1", "infernal_forge", "china_town_day", "islands_sniper", "FortFieldBuild", "monster_hunter", "paladin_castle", "Spleef_Arena_2", "Bota_campaign4", "CampaignLoading", "Developer_Scene", "christmas_train", "Space_campaign3", "Ice_Palace_Duel", "clan_fortress01", "Christmas_Town3", "orbital_station", "Duel_ghost_town", "Swamp_campaign3", "WalkingFortress", "office_christmas", "Spooky_Lunapark3", "knife3_christmas", "Portal_Campaign4", "Arena_Underwater", "emperors_palace2", "hurricane_shrine", "Castle_campaign3", "christmas_town_22", "CampaignChooseBox", "Christmas_Dinner2", "Dungeon_dead_city", "aqua_park_sandbox", "Stadium_deathmatch", "AuthorizationScene", "sky_islands_updated", "LevelToCompleteProm", "sky_islands_sandbox", "AuthenticationScene", "NuclearCity_entering", "DownloadAssetBundles", "red_light_team_fight", "freeplay_city_summer", "four_seasons_updated", "tokyo_3018_campaign4", "COLAPSED_CITY_sniper", "ice_palace_christmas", "LoveIsland_deathmatch", "cubic_arena_campaign4", "Christmas_Town_Night3", "toy_factory_christmas", "battle_royale_arcade_2", "Dungeon_magical_valley", "Death_Escape_campaign4", "battle_royale_arcade_3", "battle_royale_09_summer", "WalkingFortress_campaign4" };
+bool recoil;
+float recoilVal;
+
+int(*get_Width)();
+int(*get_Height)();
+void Pointers() {
+
+}
+
+
+int isGame(JNIEnv *env, jstring appDataDir) {
+    if (!appDataDir)
+        return 0;
+    const char *app_data_dir = env->GetStringUTFChars(appDataDir, nullptr);
+    int user = 0;
+    static char package_name[256];
+    if (sscanf(app_data_dir, "/data/%*[^/]/%d/%s", &user, package_name) != 2) {
+        if (sscanf(app_data_dir, "/data/%*[^/]/%s", package_name) != 1) {
+            package_name[0] = '\0';
+            LOGW(OBFUSCATE("can't parse %s"), app_data_dir);
+            return 0;
+        }
+    }
+    if (strcmp(package_name, GamePackageName) == 0) {
+        LOGI(OBFUSCATE("detect game: %s"), package_name);
+        game_data_dir = new char[strlen(app_data_dir) + 1];
+        strcpy(game_data_dir, app_data_dir);
+        env->ReleaseStringUTFChars(appDataDir, app_data_dir);
+        return 1;
+    } else {
+        env->ReleaseStringUTFChars(appDataDir, app_data_dir);
+        return 0;
+    }
+}
+
+void(*oldApplyRecoil)(void* obj);
+void ApplyRecoil(void* obj){
+    if(obj != nullptr){
+        LOGE("APPLY EXISTS!");
+        if(recoil){
+
+            void* aim = *(void**)((uint64_t) obj + 0x10);
+            if(aim != nullptr){
+                LOGE("AIM EXISTS!");
+                *(float*)((uint64_t) aim + 0x8) = recoilVal;
+                *(float*)((uint64_t) aim + 0xC) = recoilVal;
+                *(float*)((uint64_t) aim + 0x10) = recoilVal;
+                *(float*)((uint64_t) aim + 0x14) = 0;
+            }
+        }
+    }
+    oldApplyRecoil(obj);
+}
+
+int glHeight, glWidth;
+bool setupimg;
+
+HOOKAF(void, Input, void *thiz, void *ex_ab, void *ex_ac) {
+    origInput(thiz, ex_ab, ex_ac);
+    ImGui_ImplAndroid_HandleInputEvent((AInputEvent *)thiz);
+    return;
+}
+
+void Hooks() {
+    HOOK("0x1D50130", ApplyRecoil, oldApplyRecoil);
+}
+
+void Patches(){
+
+}
+
+void DrawMenu(){
+    static ImVec4 clear_color = ImVec4(0.45f, 0.55f, 0.60f, 1.00f);
+    {
+        ImGui::Begin(OBFUSCATE("Critical Ops 1.0a (23.1) - chr1s#4191 && networkCommand()#7611 && ohmyfajett#3500"));
+        if (ImGui::Button(OBFUSCATE("Join Discord")))
+        {
+            //isDiscordPressed = true;
+        }
+        ImGui::TextUnformatted("Its Recommended to join the discord server for mod updates etc.");
+        if (ImGui::CollapsingHeader(OBFUSCATE("Legit Mods"))) {
+            ImGui::Checkbox(OBFUSCATE("Change Recoil"), &recoil);
+            if(recoil){
+
+                recoil = false;
+                ImGui::SliderFloat(OBFUSCATE("Recoil Intensity"), &recoilVal, 0.0f, 100.0f);
+            }
+        }
+        if (ImGui::CollapsingHeader(OBFUSCATE("Rage Mods"))) {
+
+        }
+        if (ImGui::CollapsingHeader(OBFUSCATE("Visual Mods"))) {
+
+        }
+        Patches();
+        ImGui::End();
+    }
+}
+
+void SetupImgui() {
+    IMGUI_CHECKVERSION();
+    ImGui::CreateContext();
+    ImGuiIO &io = ImGui::GetIO();
+    io.DisplaySize = ImVec2((float) 2560.0, (float) 1440.0);
+    ImGui_ImplOpenGL3_Init("#version 100");
+    ImGui::StyleColorsDark();
+    ImGui::GetStyle().ScaleAllSizes(6.0f);
+    io.Fonts->AddFontFromMemoryTTF(Roboto_Regular, 30, 30.0f);
+}
+
+EGLBoolean (*old_eglSwapBuffers)(EGLDisplay dpy, EGLSurface surface);
+EGLBoolean hook_eglSwapBuffers(EGLDisplay dpy, EGLSurface surface) {
+    eglQuerySurface(dpy, surface, EGL_WIDTH, &glWidth);
+    eglQuerySurface(dpy, surface, EGL_HEIGHT, &glHeight);
+
+
+    if (!setupimg) {
+        SetupImgui();
+        setupimg = true;
+    }
+
+    ImGuiIO &io = ImGui::GetIO();
+
+    ImGui_ImplOpenGL3_NewFrame();
+    ImGui::NewFrame();
+
+    DrawMenu();
+
+    ImGui::EndFrame();
+    ImGui::Render();
+    glViewport(0, 0, (int)2560, (int)1440);
+    ImGui_ImplOpenGL3_RenderDrawData(ImGui::GetDrawData());
+    return old_eglSwapBuffers(dpy, surface);
+}
+
+void *hack_thread(void *arg) {
+    do {
+        sleep(1);
+        g_il2cppBaseMap = KittyMemory::getLibraryBaseMap("libil2cpp.so");
+    } while (!g_il2cppBaseMap.isValid());
+    auto eglhandle = dlopen("libunity.so", RTLD_LAZY);
+    auto eglSwapBuffers = dlsym(eglhandle, "eglSwapBuffers");
+    DobbyHook((void*)eglSwapBuffers,(void*)hook_eglSwapBuffers,
+              (void**)&old_eglSwapBuffers);
+    void *sym_input = DobbySymbolResolver(("/system/lib/libinput.so"), ("_ZN7android13InputConsumer21initializeMotionEventEPNS_11MotionEventEPKNS_12InputMessageE"));
+    if (NULL != sym_input) {
+        DobbyHook(sym_input,(void*)myInput,(void**)&origInput);
+    }
+    LOGI("Draw Done!");
+    return nullptr;
+}
